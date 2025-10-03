9a04aa48a808bc6877de82641552b687b5cadcce
yeqing <yeqing20021012@163.com>
2025-10-03 22:23:58 +0700

    first commit

---
+++ Assets/Challenge 1/Scripts/SpinPropeller.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinPropeller : MonoBehaviour
+
+{
+    public float spinSpeed = 1000.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, spinSpeed * Time.deltaTime);
+    }
+}
